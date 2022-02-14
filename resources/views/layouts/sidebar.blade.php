d4fdd0b9dac3f4e14c1e5261d1e6cbda2b8ed071
thien314 <macthien2302@gmail.com>
2022-02-14 17:51:01 +0700

    add ui

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,64 @@
+    <div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
+        <div class="nano">
+            <div class="nano-content">
+                <ul>
+                    <div class="logo"><a href="index.html">
+                            <!-- <img src="assets/images/logo.png" alt="" /> --><span>Focus</span></a></div>
+                    <li class="label">Main</li>
+                    <li><a class="sidebar-sub-toggle"><i class="ti-home"></i> Dashboard <span
+                                class="badge badge-primary"></span> <span
+                                class="sidebar-collapse-icon "></span></a>
+                    </li>
+
+                    <li class="label">Apps</li>
+                    <li><a class="sidebar-sub-toggle"><i class="ti-bar-chart-alt"></i> Charts <span
+                                class="sidebar-collapse-icon ti-angle-down"></span></a>
+                        <ul>
+                            <li><a href="chart-sparkline.html">Sparkle</a></li>
+                            <li><a href="chart-knob.html">Knob</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="app-event-calender.html"><i class="ti-calendar"></i> Calender </a></li>
+                    <li><a href="app-email.html"><i class="ti-email"></i> Email</a></li>
+                    <li><a href="app-profile.html"><i class="ti-user"></i> Profile</a></li>
+                    <li><a href="app-widget-card.html"><i class="ti-layout-grid2-alt"></i> Widget</a></li>
+                    <li class="label">Features</li>
+                    <li><a class="sidebar-sub-toggle"><i class="ti-layout"></i> UI Elements <span
+                                class="sidebar-collapse-icon ti-angle-down"></span></a>
+                        <ul>
+                            <li><a href="ui-typography.html">Typography</a></li>
+                            <li><a href="ui-alerts.html">Alerts</a></li>
+                            <li><a href="ui-tab.html">Tab</a></li>
+
+                        </ul>
+                    </li>
+                    <li><a class="sidebar-sub-toggle"><i class="ti-panel"></i> Components <span
+                                class="sidebar-collapse-icon ti-angle-down"></span></a>
+                        <ul>
+                            <li><a href="uc-calendar.html">Calendar</a></li>
+                            <li><a href="uc-carousel.html">Carousel</a></li>
+
+                            <li><a href="uc-rating-bar-rating.html">Bar Rating</a></li>
+                            <li><a href="uc-rating-jRate.html">jRate</a></li>
+                        </ul>
+                    </li>
+                  
+                    <li><a class="sidebar-sub-toggle"><i class="ti-heart"></i> Icons <span
+                                class="sidebar-collapse-icon ti-angle-down"></span></a>
+                        <ul>
+                            <li><a href="font-themify.html">Themify</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="sidebar-sub-toggle"><i class="ti-map"></i> Maps <span
+                                class="sidebar-collapse-icon ti-angle-down"></span></a>
+                        <ul>
+                            <li><a href="gmaps.html">Basic</a></li>
+                            <li><a href="vector-map.html">Vector Map</a></li>
+                        </ul>
+                    </li>
+                   
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- /# sidebar -->
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('my-home', 'HomeController@myHome');
+Route::get('my-users', 'HomeController@myUsers');
